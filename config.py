7e046d9b40c2206ef63dbb2ac1999c6f7538d10f
mnjegovan-lab <76666528+mnjegovan-lab@users.noreply.github.com>
2021-01-22 13:52:15 +0100

    ODBC driver change

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'mnjegovan'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Sefc321!'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=SQL+Serve+Native+Client+10.0'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     #'?driver=ODBC+Driver+10+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
