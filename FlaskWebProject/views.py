2b1814e46574a5db16a4a4d24e47547a20a396b9
mnjegovan-lab <76666528+mnjegovan-lab@users.noreply.github.com>
2021-01-31 21:04:37 +0100

    Update views.py
    logging added

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -61,11 +61,13 @@ def post(id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+        app.logger.info('Login was SUCCESSFULL!')
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning('Login attempt FAILED!')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
